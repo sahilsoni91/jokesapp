753fc6e44805ff9ce4611373fd001b25cad78ebd
Sahil Soni <E01602@hsc.com>
2019-12-27 11:17:54 +0530

    Java Based Configuration to get dependency

---
+++ src/main/java/app/jokes/jokesapp/config/ChuckConfiguration.java
@@ -0,0 +1,15 @@
+package app.jokes.jokesapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+
+@Configuration
+public class ChuckConfiguration {
+
+	@Bean
+	public ChuckNorrisQuotes chuckNorrisQuotes() {
+		return new ChuckNorrisQuotes();
+	}
+}
